0820adb40ee95500750814705b98888f64c25f77
vkomusar <vjekoslav91@gmail.com>
2021-02-16 00:43:12 +0100

    WIP navigation & CSS fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-(function($) {
-  $(function() {
-    console.log("Ready!");
+document.addEventListener('DOMContentLoaded', init, false);
+function init(){
+  var button = document.getElementById('toggleBtn');
+  button.addEventListener('click', function() {
+
+    //check if checkbox is checked if NOT add class
+    var noScroll = document.body;
+    noScroll.classList = 'noScroll';
+    //if checkbox is checked REMOVE class
+
   });
-})(jQuery);
+};
+
